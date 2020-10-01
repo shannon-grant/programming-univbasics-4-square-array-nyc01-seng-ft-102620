b0102df585eedd02527c3890bcbb141870740c87
Shannon Grant <grant.shannon90@gmail.com>
2020-10-01 23:00:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,8 @@
 def square_array(numbers)
   count = 0
-  new_array = []
 
   while count < numbers.length do
     count = numbers[count] * numbers[count]
-    new_array << count
     count +=1
   end
-  new_array
 end
